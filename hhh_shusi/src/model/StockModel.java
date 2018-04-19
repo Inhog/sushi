a21ad8c69e5016621554f1ecac17b0fa0bb282f7
mrclap <mr.clap8@gmail.com>
2018-04-19 15:17:35 +0900

    stockmodel 아주 조금 추가 됨.

--- hhh_shusi/src/model/StockModel.java
+++ hhh_shusi/src/model/StockModel.java
@@ -1,17 +1,29 @@
 package model;
 
-public class StockModel {
+import java.sql.Connection;
 
+public class StockModel {
+	static Connection con;
+	String url;
+	String user;
+	String pass;
 	
+	public void dbConnection(){
+		try {
+			con = DBconnection.getConnection();
+		} catch (Exception e) {
+			System.out.println("StockModel DB연결 실패" + e.getMessage());// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	//재고 입력
 	public void insert(){
-		
 	}
 	//재고 수정
 	public void modify(){
 			
 	}
 	public void delete(){
-		
 	}
+	
 }
